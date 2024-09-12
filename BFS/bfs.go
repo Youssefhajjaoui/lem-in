4bebfd30982a9037ca657449323bd5c67012656c
Oussamabenali4 <oussama.benali@ump.ac.ma>
2024-09-12 21:53:22 +0100

    initilizing the repo

---
+++ BFS/bfs.go
@@ -0,0 +1,8 @@
+package bfs 
+
+
+type Vertex struct{
+	name string
+	crd [2]int
+	adjacentVertices []*Vertex 
+}
---
+++ parse_file/parse_file.go
@@ -0,0 +1,13 @@
+package parse_file 
+
+
+// this file contains function to: 
+// 1- get the name of the file from the user
+// 2- open and read the file
+// 3- split the data of the file into 3 
+// // starting room 
+// // middle rooms
+// // end room 
+// // links between rooms
+// every function should return the error type 
+// so it can be used by others
