a18f13a09834ce9d8a61251c7b238ed83f3c58b2
Oussamabenali4 <oussama.benali@ump.ac.ma>
2024-09-13 20:34:36 +0100

    add the staring and the ending room to the graph

--- BFS/bfs.go
+++ BFS/bfs.go
@@ -35,16 +35,19 @@ func (v *Vertex)include(vertex *Vertex)bool{
 //////////////////////////////////////////////////////////////
 type Graph struct{
 	Verteces []*Vertex
+	Start *Vertex // the starting room
+	End *Vertex // the ending room
 }
 
 func NewGraph()*Graph{
-	return &Graph{Verteces : []*Vertex{}}
+	return &Graph{Verteces : []*Vertex{}, Start : nil , End : nil }
 }
 
 
-func (g *Graph)Add(v *Vertex){
+func (g *Graph)AddRoom(v *Vertex){
 	g.Verteces = append(g.Verteces, v)
 }
+
 
 
 
