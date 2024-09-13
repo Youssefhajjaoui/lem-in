6a649917b927e14eaf14fd4387ec1f2ddf6ab862
Oussamabenali4 <oussama.benali@ump.ac.ma>
2024-09-13 19:28:29 +0100

    adding a basic bfs package

--- BFS/bfs.go
+++ BFS/bfs.go
@@ -2,7 +2,34 @@ package bfs
 
 
 type Vertex struct{
-	name string
-	crd [2]int
-	adjacentVertices []*Vertex 
+	// name of the room
+	Name string
+	// cordonate of the room
+	//crd [2]int
+	// liks from the current room to other rooms
+	adjacentVerteces []*Vertex 
+}
+
+
+
+func New(name string)*Vertex{
+    return &Vertex{Name : name}
+}
+
+// this is to relate verticies
+func (v *Vertex)Add_adjacent_vertex(vertex *Vertex){
+	if v.include(vertex){
+		return
+	}
+	v.adjacentVerteces = append(v.adjacentVerteces, vertex)
+	vertex.Add_adjacent_vertex(v)
+}
+// check if if the v is already related to vertex
+func (v *Vertex)include(vertex *Vertex)bool{
+    for _, e := range v.adjacentVerteces {
+        if e == vertex {
+            return true
+        }
+    }
+    return false
 }
