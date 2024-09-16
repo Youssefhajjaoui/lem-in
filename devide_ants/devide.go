5252600cfef1e1c5070a6671df51a7f213158c4e
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2024-09-16 11:18:16 +0100

    update:

---
+++ devide_ants/devide.go
@@ -0,0 +1,13 @@
+package devide 
+
+/*L1-3 L2-2
+L1-4 L2-5 L3-3
+L1-0 L2-6 L3-4
+L2-0 L3-0*/
+
+func Devide(ants int , paths [][]string)/*map[string]int*/{
+	// send all the ans in the same derection// 
+	for i:= len(paths) -1 ; i >= 0 ; i --  {
+		fmt.Printf("L%s-%s", paths[0][len(paths[0]-1],paths[0][i] )
+	}
+}
