432cad3ce699442c98b73ab77fbf83878db1b0f3
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2024-09-18 11:24:50 +0100

    tempreray commit

--- parse_file/parse_file.go
+++ parse_file/parse_file.go
@@ -3,38 +3,53 @@ package parse_file
 import (
 	"bufio"
 	"errors"
-
 	//	"fmt"
 	"os"
 	"strings"
-	// bfs "lem-in/BFS"
 )
 
-// this file contains function to:
-// 1- get the name of the file from the user
-// 2- open and read the file
-// 3- split the data of the file into 3
-// // starting room
-// // middle rooms
-// // end room
-// // links between rooms
-// every function should return the error type
-// so it can be used by others
+type Nest struct {
+	Rooms []string
+	Tunels [2]string
+	Start []string
+	End []string
+	Ants int
+}
 
-func Getdata(filename string) ([]string, error) {
-	result := []string{}
+
+func FillTheNest(filename string) ([]string, error) {
+
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, errors.New("error from open")
 	}
-	Scanner := bufio.NewScanner(file)
 	defer file.Close()
+
+	result := []string{}
+	Scanner := bufio.NewScanner(file)
 	for Scanner.Scan() {
-		line := Scanner.Text()
+		line := strings.TrimSpace(Scanner.Text())
+		if len(line) == 0 {continue}
 		result = append(result, line)
 	}
-	return result, nil
+	nest := Parse(result)
+	//return result, nil
 }
+
+func Parse(result []string)(Nest, error){
+	if len(result == 0){
+		return Nest{}, errors.New("empty file")
+	}
+	var nest Nest
+	nest.Ants , err := strconv.Atoi(result[0])
+	if err != nil{
+		return Nest{}
+	}
+	for i := 1 ; i < len(result []string) ; i ++ {
+	}
+}
+
+
 
 func GetNodes(arr []string) ([]string, error) {
 	Nodes := []string{}
@@ -46,32 +61,16 @@ func GetNodes(arr []string) ([]string, error) {
 	return Nodes, nil
 }
 
-// func ParsetoNode(arr []string) ([]*Vertex, error) {
-// 	values, err := GetNodes(arr)
-// 	if err != nil {
-// 		return nil, errors.New("NO Vertexes")
-// 	}
-// 	Nodes := []*Vertex{}
-// 	for i := 0; i < len(values); i++ {
-// 		g.Verteces = append(g.Verteces, &Vertex{
-// 			Name: values[i],
-// 		})
-// 	}
-// 	if len(Nodes) == 0 {
-// 		return nil, errors.New("error in the Nodes details")
-// 	}
-// 	return Nodes, nil
-// }
 
 func GetEdges(arr []string) ([][]string, error) {
 	cols := [][]string{}
-	rows := []string{}
 	for _, v := range arr {
-		if Len := len(strings.Split(v, "-")); Len == 2 {
-			rows = append(rows, strings.Split(v, "-")[0])
-			rows = append(rows, strings.Split(v, "-")[1])
+		rows := []string{}
+		Len := strings.Split(v, "-")
+		if len(Len) == 2 {
+			rows = append(rows, Len[0])
+			rows = append(rows, Len[1])
 			cols = append(cols, rows)
-			rows = []string{}
 		}
 	}
 	if len(cols) == 0 {
@@ -83,7 +82,8 @@ func GetEdges(arr []string) ([][]string, error) {
 func GetStart(arr []string) string {
 	for i, v := range arr {
 		if v == "##start" {
-			return strings.Split(arr[i+1], "")[0]
+			//return strings.Split(arr[i+1], " ")[0]
+			return arr[i+1]
 		}
 	}
 	return ""
@@ -92,32 +92,8 @@ func GetStart(arr []string) string {
 func GetEnd(arr []string) string {
 	for i, v := range arr {
 		if v == "##end" {
-			return strings.Split(arr[i+1], "")[0]
+			return strings.Split(arr[i+1], " ")[0]
 		}
 	}
 	return ""
 }
-
-// func ProcessInput(filename string) (*Graphs, error) {
-// 	g := &Graphs{}
-// 	input, err := Getdata(filename)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	Nodes, err := GetNodes(input)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	Rooms, err := g.ParsetoNode(Nodes)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if err := g.GetEdges(input, Rooms); err != nil {
-// 		return nil, err
-// 	}
-// 	//	fmt.Println(g->adjacentVerteces)
-// 	//
-// 	// fmt.Println(g)
-// 	//
-// 	return g, nil
-// }
