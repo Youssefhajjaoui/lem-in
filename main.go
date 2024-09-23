f0d3ee8ca7ac35e71079331aa7655d21c800cb11
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2024-09-23 18:17:49 +0100

    lost

--- main.go
+++ main.go
@@ -38,13 +38,56 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	graph.AllPaths()
-	all := graph.All
-	fmt.Println("roods found: ", all)
+
+	/*oussama*/ 
+	all, err  := graph.FindAllWays()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("oussama: roods found: ", all)
 	mat, err := devide.Devide(all, nest.Ants)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	devide.Print(mat)
+	maxFlow, paths  := graph.EdmondsKarp()
+
+	fmt.Printf("The maximum possible flow is %d\n", maxFlow)
+	fmt.Println("paths taking")
+	fmt.Println(paths)
+	graph.AllPaths() 
+	fmt.Println("try")
+	all, err  = graph.FindAllWays()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("oussama: roods found: ", all)
+	mat, err = devide.Devide(all, nest.Ants)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	devide.Print(mat)
+	//maxFlow, paths  = graph.EdmondsKarp()
+	fmt.Println("paths")
+	fmt.Println(paths)
+
+	/*youcef*/ /* 
+	graph.AllPaths()
+	all := graph.All
+	fmt.Println("youcef: roods found: ", all)
+	mat, err := devide.Devide(all, nest.Ants)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	devide.Print(mat)
+	maxFlow, paths  := graph.EdmondsKarp()
+	fmt.Printf("The maximum possible flow is %d\n", maxFlow)
+	fmt.Println("paths taking")
+	fmt.Println(paths)
+	*/
 }
